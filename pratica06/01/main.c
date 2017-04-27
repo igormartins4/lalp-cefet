a19e547393e33d71b065bb22acea0562d291e071
Igor Martins <iguinhoigor4@gmail.com>
2017-04-27 10:25:16 -0300

    Prática 6 adicionada
    Alguns programas não foram finalizados ou estão com bugs. Serão corrigidos em pouco tempo.

---
+++ pratica06/01/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+    int a[10], x, i, j, achei=0;
+
+    setlocale(LC_ALL, "Portuguese");
+    for(i=0; i<10; i++) {
+        printf("\nDigite o %dº número inteiro: \n", i+1);
+        scanf("%d", &a[i]);
+    }
+    printf("\nDigite o número que será comparado: \n");
+    scanf("%d", &x);
+    for(j=0; j<10; j++) {
+        if(a[i] == x) {
+            achei=1;
+        }
+    }
+    if(achei = 1) {
+        printf("\nACHEI\n");
+    }
+    else {
+        printf("\nNÃO ACHEI\n");
+    }
+
+    return 0;
+}
